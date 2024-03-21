618bf6ab29ff3ba1711c22771a337889c414f72b
‘TX7’ <1224745953@qq.com>
2024-03-21 13:18:51 +0800

    3-21

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,8 @@ import theme from "./theme.js";
 
 export default defineUserConfig({
   base: "/",
-
+  host:'localhost',
+  port:4396,
   lang: "zh-CN",
   title: "博客演示",
   description: "vuepress-theme-hope 的博客演示",
--- src/test/test.js
+++ src/test/test.js
@@ -1,35 +1,10 @@
-function wrap(promise) {
-  // 在这里包装一个 promise，可以控制原来的promise是成功还是失败
-  let abort;
-  let newPromise = new Promise((resolve, reject) => {
-    // defer 方法
-    abort = reject;
-  });
-  let p = Promise.race([promise, newPromise]); // 任何一个先成功或者失败 就可以获取到结果
-  // 把自定义的promise的reject方法暴露给p
-  p.abort = abort;
-  return p;
-}
-
-// 测试用例
-const promise = new Promise((resolve, reject) => {
+const fn = async (val, delay) => {
   setTimeout(() => {
-    // 模拟的接口调用 ajax 肯定有超时设置
-    resolve("成功");
-  }, 4000);
-});
+    console.log(val);
+  }, delay);
+  return val;
+};
 
-let newPromise = wrap(promise);
-
-setTimeout(() => {
-  // 超过3秒 就算超时 应该让 proimise 走到失败态
-  newPromise.abort("超时了");
-}, 3000);
-
-newPromise
-  .then((data) => {
-    console.log("成功的结果" + data);
-  })
-  .catch((e) => {
-    console.log("失败的结果" + e);
-  });
+const first = await fn(1, 1000);
+const sec = await fn(first+1, 2000);
+const thrid = await fn(sec+1, 3000);
